c225f3ccd9c49190cdf9f0e3598b197b2cec7ef4
ligao <ligao913@163.com>
2023-05-25 08:41:56 +0800

    and shake class method

--- shake/src/main/java/cn/lizii/shake/LZShake.java
+++ shake/src/main/java/cn/lizii/shake/LZShake.java
@@ -1,4 +1,15 @@
 package cn.lizii.shake;
 
+import android.content.Context;
+import android.os.Vibrator;
+
 public class LZShake {
+    public String getVersion(){
+        return "--";
+    }
+
+    public void shake(Context context,int timeMs){
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(timeMs);
+    }
 }
